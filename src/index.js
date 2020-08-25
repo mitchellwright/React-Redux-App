d61ae667d8e934809edd4c50e65eeabbb8d75cc6
Mitchell Wright <mitchellbwright@gmail.com>
2020-08-25 17:42:50 -0600

    set up store and Provider

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+import "./tailwind.output.css";
+
+function hotsReducer() {
+  return {
+    title: "Hello world! I'm in the Redux store!",
+  };
+}
+
+const store = createStore(hotsReducer);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  </Provider>,
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  document.getElementById("root")
+);
